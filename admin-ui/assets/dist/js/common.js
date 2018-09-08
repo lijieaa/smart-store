df839aab211c5202b0f6cf08293e6d7e17a52996
liuhong-summer <1150487420@qq.com>
2018-09-08 22:17:05 +0800

    验证

--- admin-ui/assets/dist/js/common.js
+++ admin-ui/assets/dist/js/common.js
@@ -2,6 +2,14 @@ $(".rest-drop>a").click(function () {
     $(this).next().slideToggle();
 })
 
-$(".rest-drop>ul>li").click(function() {
+jQuery.validator.addMethod("isMobile", function(value, element) {
+    var length = value.length;
+    var mobile = /^(13[0-9]{9})|(18[0-9]{9})|(19[0-9]{9})|(16[0-9]{9})|(14[0-9]{9})|(17[0-9]{9})|(15[0-9]{9})$/;
+    return this.optional(element) || (length == 11 && mobile.test(value));
+}, "请正确填写您的手机号码");
+
+$.extend($.validator.messages, {
+    required: "这是必填字段",
+    digits: '只能输入数字'
 
 })
